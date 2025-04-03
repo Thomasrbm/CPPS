7078475ac55686190d6cd0f26e77754177ae5ad8
Thomas Robert <throbert@c1r4p9.42lehavre.fr>
2025-04-03 14:12:13 +0200

    ex00 du 0

---
+++ cpp0/ex01/PhoneBook.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: throbert <throbert@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/03 08:05:43 by throbert          #+#    #+#             */
+/*   Updated: 2025/04/03 08:09:15 by throbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+class phonebook
+{
+public:
+	std::;
+
+private:
+	std::string phonenumber;
+	std::string	lastname;
+	std::string	nickname;
+	std::string	phonenumber;
+	std::string	darksecret;
+}
